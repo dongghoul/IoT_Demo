15db9cf8b56d2f1d3722694cc3b42a82b2172e15
dongghoul <78191012+dongghoul@users.noreply.github.com>
2023-03-20 09:38:15 +0700

    demo

---
+++ Demo/Core/Inc/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *
+ *  Created on: Mar 20, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer1_flag;
+
+#endif /* INC_TIMER_H_ */
---
+++ Demo/Debug/Core/Src/schedular.d
@@ -0,0 +1,3 @@
+Core/Src/schedular.o: ../Core/Src/schedular.c ../Core/Inc/schedular.h
+
+../Core/Inc/schedular.h:
